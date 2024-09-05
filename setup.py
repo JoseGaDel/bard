1f4d161ceeb632611e764c79d0ac5810b2ff96d0
JoseGaDel <157685260+JoseGaDel@users.noreply.github.com>
2024-09-05 17:35:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url="https://github.com/JoseGaDel/bard",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
